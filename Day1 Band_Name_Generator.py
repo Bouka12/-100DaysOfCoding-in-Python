480948f702b1c2f810484f42d232d2cf92fa6667
Mabrouka Salmi <61268036+Bouka12@users.noreply.github.com>
2022-01-15 17:40:21 +0100

    Add files via upload

---
+++ Day1 Band_Name_Generator.py
@@ -0,0 +1,27 @@
+# Day 1 - #100DaysOfCoding Challenge, I aim to brush up on my coding skills on Python and practice to be a Python Developer (one of 2022 Goals). As I am a PhD student programming skills are highly important for that alleviate my skills on python is the first step.
+
+# Today following Angela 100 days of coding challenge 
+# Day 1 I went through :
+# Printing
+# Strings
+# Debugging
+# Python Input Function
+# Variables and Variables naming in Python
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator!")
+#2. Ask the user for the city that they grew up in.
+city_name = print(input(" What's name of the city you grew up in? "))
+#3. Ask the user for the name of a pet.
+pet_name = print(input("What's your pet's name? "))
+#4. Combine the name of their city and pet and show them their band name.
+print("Your Band Name could be:\n "+ city_name + " "+ pet_name)
+# print("Your band name could be: \n", input(" What's name of the city you grew up in? \n") + input("What's your pet's name? \n"))
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
+
+# Another Way to do so
+# Greeting for the program
+print("Welcome to the Band Name Generator!")
+# Concatenate all the steps in one line of code:
+print("Your band name could be: \n", input(" What's name of the city you grew up in? \n") + input("What's your pet's name? \n"))
